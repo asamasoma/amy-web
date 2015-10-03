c7676d940d0a1ba772c60ff1b3834c870a79209b
asamasoma <amasa.amos@intentmedia.com>
2015-10-03 15:32:39 -0400

    Add additional videos to carousel.

--- src/app/extra/extra.controller.js
+++ src/app/extra/extra.controller.js
@@ -7,22 +7,42 @@
     .filter('trustAsResourceUrl', ['$sce', function($sce) {
 		return function(url) {
 		return $sce.trustAsResourceUrl(url);
-	}}]);
+	};}]);
 
   /** @ngInject */
   function ExtraController($scope) {
   	$scope.myInterval = 5000;
-  	$scope.noWrapSlides = false;
-  	var slides = $scope.slides = [];
-  	$scope.addSlide = function() {
-    	slides.push({
-      	url: 'https://player.vimeo.com/video/137779029',
-      	link: 'https://vimeo.com/137779029',
-      	text: 'DELL National Commercial (30 second spot)'
-    	});
-  	};
-  	for (var i=0; i<4; i++) {
-    	$scope.addSlide();
-  	}
+  	$scope.noWrapSlides = false; 
+  	$scope.slides = [
+      {
+        url:  'https://player.vimeo.com/video/137779029',
+        link: 'https://vimeo.com/137779029',
+        text: 'DELL National Commercial (30 second spot)'
+      },
+      {
+        url:  'https://player.vimeo.com/video/137779028',
+        link: 'https://vimeo.com/137779028',
+        text: 'DELL National Commercial (60 second spot)'
+      },
+      {
+        url:  'https://player.vimeo.com/video/137779034',
+        link: 'https://vimeo.com/137779034',
+        text: 'Verizon Wireless National Commercial: "Employee of the Month"'
+      },
+      {
+        url:  'https://player.vimeo.com/video/137779030',
+        link: 'https://vimeo.com/137779030',
+        text: 'SATAN HATES YOU, directed by James Felix McKenney'
+      },
+      {
+        url:  'https://player.vimeo.com/video/137779027',
+        link: 'https://vimeo.com/137779027',
+        text: 'SALON, directed by ManSee Kong'
+      },
+      {
+        url:  'https://player.vimeo.com/video/137779031',
+        link: 'https://vimeo.com/137779031',
+        text: 'SMOKE GETS IN YOUR EYES, directed by Alexander Lau'
+      }];
   }
 })();
