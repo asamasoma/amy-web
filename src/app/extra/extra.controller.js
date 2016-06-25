96797f0306697a9bf343b47bb61e9c8ae684a81a
asamasoma <amasa.amos@intentmedia.com>
2016-06-25 17:06:26 -0400

    Add vimeo links and fix title

--- src/app/extra/extra.controller.js
+++ src/app/extra/extra.controller.js
@@ -15,6 +15,21 @@
   	$scope.noWrapSlides = false; 
   	$scope.slides = [
       {
+        url:  'https://player.vimeo.com/video/171354973',
+        link: 'https://vimeo.com/171354973',
+        text: 'TD BANK Commercial: Meet B.A.R.T. (60 sec)'
+      },
+      {
+        url:  'https://player.vimeo.com/video/171977893',
+        link: 'https://vimeo.com/171977893',
+        text: 'TD BANK Commercial: We’re still open, B.A.R.T (30 sec)'
+      },
+      {
+        url:  'https://player.vimeo.com/video/169111520',
+        link: 'https://vimeo.com/169111520',
+        text: 'CUSTOMER #935 - At the laundromat'
+      },
+      {
         url:  'https://player.vimeo.com/video/137779029',
         link: 'https://vimeo.com/137779029',
         text: 'DELL National Commercial (30 second spot)'
@@ -28,6 +43,11 @@
         url:  'https://player.vimeo.com/video/137779034',
         link: 'https://vimeo.com/137779034',
         text: 'Verizon Wireless National Commercial: "Employee of the Month"'
+      },
+      {
+        url:  'https://player.vimeo.com/video/169151392',
+        link: 'https://vimeo.com/169151392',
+        text: 'TRIVIA NIGHT - Where the magic happens'
       },
       {
         url:  'https://player.vimeo.com/video/137779030',
